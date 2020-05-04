97c0e0e1b5c602a0ddfe0e1e4191612f0ca16824
hayeon17kim <qwndhs@gmail.com>
2020-05-04 19:05:58 +0900

    updating app with a shop page, collection preview component and collection item component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -6,7 +6,7 @@ import "./menu-item.styles.scss";
 const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
   <div
     className={`${size} menu-item`}
-    onClick={() => history.push(`${match.url}&${linkUrl}`)}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
   >
     <div
       className="background-image"
@@ -21,4 +21,4 @@ const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
   </div>
 );
 
-export default withRouter(MenuItem);
+export default withRouter(MenuItem); // powered up component=> now we have access to history
